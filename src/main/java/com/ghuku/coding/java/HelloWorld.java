12cbc2b1d8f429ef3cdea5dffca79f9cde01fbb7
bhadreswar-ghuku <ghukubhadreswar@gmail.com>
2016-03-20 20:42:30 +0530

    HelloWorld program

---
+++ src/main/java/com/ghuku/coding/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ghuku.coding.java;
+
+/**
+ * Created by bhadreswar.ghuku on 20/3/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java, Intellij And GitHub");
+    }
+}
